fbd0707d3ab1b3e11eaa24a3158771d3695d5544
Tatiana Gorelova <mogila2001@gmail.com>
2024-02-05 20:02:52 +0300

    fix bug in Calendar store decorator

--- client/src/shared/config/storybook/StoreDecorator/CalendarStoreDecorator.tsx
+++ client/src/shared/config/storybook/StoreDecorator/CalendarStoreDecorator.tsx
@@ -2,8 +2,7 @@ import { ReducersMapObject } from '@reduxjs/toolkit'
 import { Decorator } from '@storybook/react'
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider'
 import { calendarPageReducer } from 'pages/CalendarPage'
-import { stateAllErrors, stateAllIn } from './state'
-import { stateLoginFormLoading } from './LoginFormStoreDecorator'
+import { stateAllErrors, stateAllIn, stateAllIsLoading } from './state'
 
 export const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
     CalendarPage: calendarPageReducer,
@@ -21,7 +20,7 @@ export const calendarPageIsLoadingStoreDecorator: Decorator = (Story) => (
 )
 
 export const calendarPageErrorStoreDecorator: Decorator = (Story) => (
-    <StoreProvider initialState={stateLoginFormLoading} asyncReducers={defaultAsyncReducers}>
+    <StoreProvider initialState={stateAllIsLoading} asyncReducers={defaultAsyncReducers}>
         {Story()}
     </StoreProvider>
 )
