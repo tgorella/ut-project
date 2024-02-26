95c5e53ef98bfea058c732993fb7b9e32d62c877
Tatiana Gorelova <mogila2001@gmail.com>
2024-02-26 15:22:47 +0300

    refactor: migrate events requests to graphql

--- client/src/entities/Event/model/services/fetchEvents/fetchEvents.ts
+++ client/src/entities/Event/model/services/fetchEvents/fetchEvents.ts
@@ -2,23 +2,22 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import { ThunkConfig } from 'app/providers/StoreProvider'
 import i18n from 'shared/config/i18n/i18n'
 import { Event } from '../../types/Event'
-import { getUserAuthData } from 'entities/User'
 
 
 export const fetchEvents = createAsyncThunk<Event[], void ,ThunkConfig<string>>(
     'event/fetchEvents',
     async (_, thunkAPI) => {
-        const {rejectWithValue, getState, extra} = thunkAPI
+        const {rejectWithValue, extra} = thunkAPI
         try {
-            const userData = getUserAuthData(getState())
-            const {data} = await extra.api.get<Event[]>('/events')
-            if (data[0].userId !== userData?._id) {
-                return rejectWithValue(i18n.t('Нет доступа'))
-            }
+            const {data} = await extra.api.post('/', {
+                'query': 'query Events { events { _id endTime eventDate eventType { _id color name } notes place startTime title } }',
+                'operation-name':'Events'
+            })
+           
             if (!data) {
                 throw new Error('err')
             }
-            return data
+            return data.data.events
         } catch (error) {
             return rejectWithValue(i18n.t('Клиент не найден'))
         }
