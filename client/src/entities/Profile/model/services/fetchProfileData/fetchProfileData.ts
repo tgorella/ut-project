513bf81e35890420e6ec86d224f2d4b4ce0b1457
Tatiana Gorelova <mogila2001@gmail.com>
2024-02-26 13:17:16 +0300

    refactor: migrate profile requests to graphql

--- client/src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
+++ client/src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -4,17 +4,19 @@ import i18n from 'shared/config/i18n/i18n'
 import { Profile } from '../../types/profileSchema'
 
 
-export const fetchProfileData = createAsyncThunk<Profile, string,ThunkConfig<string>>(
+export const fetchProfileData = createAsyncThunk<Profile, void,ThunkConfig<string>>(
     'profile/fetchProfileData',
-    async (userId, thunkAPI) => {
+    async (_, thunkAPI) => {
         const {rejectWithValue, extra} = thunkAPI
         try {
-            const {data} = await extra.api.get<Profile>(`/users/${userId}`)
-            
+            const {data} = await extra.api.post('/', {
+                'query': 'query Query { user { _id avatar city country currency email firstname lastOrderNumber lastname username } }',
+                'operationName': 'Query'
+            })
             if (!data) {
                 throw new Error('err')
             }
-            return data
+            return data.data.user
         } catch (error) {
             return rejectWithValue(i18n.t('Неправильные логин или пароль'))
         }
