8fdfb43ab51718b6a198f4aa94efeca491ca6cc0
Tatiana Gorelova <gorelovate@yandex.ru>
2024-04-17 22:49:25 +0300

    add tests for eventTypes services

---
+++ client/src/entities/EventType/model/services/deleteEventType/deleteEventType.test.ts
@@ -0,0 +1,33 @@
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk'
+import { deleteEventType } from './deleteEventType'
+
+describe('deleteEventType', () => {
+    test('success', async () => {
+        const data = {
+            data: {
+                deleteEventType: {
+                    id: '643c34fj3ytg43e22868a6eb63c'
+                }
+            }
+        }
+
+        const thunk = new TestAsyncThunk(deleteEventType)
+        thunk.api.post.mockReturnValue(Promise.resolve({data}))
+
+        const result = await thunk.callThunk('643c34fj3ytg43e22868a6eb63c')
+
+        expect(thunk.api.post).toHaveBeenCalled()
+        expect(result.meta.requestStatus).toBe('fulfilled')
+        expect(result.payload).toEqual(data.data.deleteEventType)
+    })
+    test('error fetch', async () => {
+
+        const thunk = new TestAsyncThunk(deleteEventType)
+        thunk.api.post.mockReturnValue(Promise.resolve({status: 403}))
+
+        const result = await thunk.callThunk('643c34fj3ytg43e22868a6eb63c')
+
+        expect(thunk.api.post).toHaveBeenCalled()
+        expect(result.meta.requestStatus).toBe('rejected')
+    })
+})
---
+++ client/src/entities/EventType/model/services/updateEventType/updateEventType.test.ts
@@ -0,0 +1,41 @@
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk'
+import { updateEventType } from './updateEventType'
+
+describe('updateEventType', () => {
+    test('success', async () => {
+
+        const data = {data: {
+            updateEventType: {
+                _id: '643c34fj3ytg43e22868a6eb63c',
+                color: '#ff759f',
+                name: 'Работа'
+            }
+        }}
+
+        const thunk = new TestAsyncThunk(updateEventType)
+        thunk.api.post.mockReturnValue(Promise.resolve({data}))
+
+        const result = await thunk.callThunk({
+            _id: '643c34fj3yt68a6eb63c',
+            color: '#ff759f',
+            name: 'Работа'
+        })
+
+        expect(thunk.api.post).toHaveBeenCalled()
+        expect(result.meta.requestStatus).toBe('fulfilled')
+        expect(result.payload).toEqual(data.data.updateEventType)
+    })
+    test('error fetch', async () => {
+        const thunk = new TestAsyncThunk(updateEventType)
+        thunk.api.post.mockReturnValue(Promise.resolve({status: 403}))
+
+        const result = await thunk.callThunk({
+            _id: '643c34fj3yt68a6eb63c',
+            color: '#ff759f',
+            name: 'Работа'
+        })
+
+        expect(thunk.api.post).toHaveBeenCalled()
+        expect(result.meta.requestStatus).toBe('rejected')
+    })
+})
