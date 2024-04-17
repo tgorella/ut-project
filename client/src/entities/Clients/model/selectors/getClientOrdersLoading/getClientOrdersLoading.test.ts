3466ac324186ef1672f5ebfddf9ee99b622d0d72
Tatiana Gorelova <gorelovate@yandex.ru>
2024-04-17 16:59:26 +0300

    add tests for client services and selectors

---
+++ client/src/entities/Clients/model/selectors/getClientOrdersLoading/getClientOrdersLoading.test.ts
@@ -0,0 +1,18 @@
+import { StateSchema } from 'app/providers/StoreProvider'
+import { getClientOrdersLoading } from './getClientOrdersLoading'
+
+describe('getClientOrdersLoading.test', () => {
+    test('should return value', () => {
+        const state: DeepPartial<StateSchema> = {
+            clientDetails: {
+                ordersLoading: true
+            }
+        }
+
+        expect(getClientOrdersLoading(state as StateSchema)).toEqual(true)
+    })
+    test('with empty state', () => {
+        const state: DeepPartial<StateSchema> = {}
+        expect(getClientOrdersLoading(state as StateSchema)).toEqual(undefined)
+    })
+})
---
+++ client/src/entities/Clients/model/services/deleteClient/deleteClient.test.ts
@@ -0,0 +1,24 @@
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk'
+import { deleteClient } from './deleteClient'
+
+describe('deleteClient.test', () => {
+    test('success', async () => {
+        const data = { data: {
+            deleteClient: '643c34fj3ytg43e22868a6eb63c'
+        }}
+
+        const thunk = new TestAsyncThunk(deleteClient)
+        thunk.api.post.mockReturnValue(Promise.resolve({data}))
+
+        const result = await thunk.callThunk('643c34fj3ytg43e22868a6eb63c')
+        expect(thunk.api.post).toHaveBeenCalled()
+        expect(result.meta.requestStatus).toBe('fulfilled')
+        expect(result.payload).toEqual(data.data.deleteClient)
+    })
+    test('error fetch', async () => {
+        const thunk = new TestAsyncThunk(deleteClient)
+        thunk.api.post.mockReturnValue(Promise.resolve({status: 403}))
+        const result = await thunk.callThunk('643c34fj3ytg43e22868a6eb63c')
+        expect(result.meta.requestStatus).toBe('rejected')
+    })
+})
