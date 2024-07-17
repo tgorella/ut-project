a130a07585cab43bb6f66d1c844abb8140a71557
Tatiana Gorelova <gorelovate@yandex.ru>
2024-07-17 11:10:34 +0300

    minor fixes

--- client/src/entities/Payment/ui/Payment.stories.tsx
+++ client/src/entities/Payment/ui/Payment.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import 'app/styles/index.scss'
-import { WithColorsTheme } from 'shared/config/storybook/ThemeDecorator/WithColorsTheme'
+import { WithColorsTheme } from '@/shared/config/storybook/ThemeDecorator/WithColorsTheme'
 import { Payment } from './Payment'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
--- client/src/entities/Payment/ui/Payment.tsx
+++ client/src/entities/Payment/ui/Payment.tsx
@@ -1,5 +1,5 @@
 import cls from './Payment.module.scss'
-import classNames from 'shared/lib/classNames/ClassNames'
+import classNames from '@/shared/lib/classNames/ClassNames'
 import {memo} from 'react'
 
 interface PaymentProps {
