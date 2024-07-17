a0fb0f20524b6083474274df36d40b1f8f604cc7
Tatiana Gorelova <gorelovate@yandex.ru>
2024-07-17 10:58:40 +0300

    payment entity

---
+++ client/src/entities/Payment/ui/Payment.tsx
@@ -0,0 +1,15 @@
+import cls from './Payment.module.scss'
+import classNames from 'shared/lib/classNames/ClassNames'
+import {memo} from 'react'
+
+interface PaymentProps {
+  className?: string;
+}
+export const Payment = memo(({className} : PaymentProps) => {
+
+    return ( 
+        <div className={classNames(cls.Payment, {}, [className])}>
+      
+        </div>
+    )
+})
