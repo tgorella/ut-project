f29c4365b4334dfcac91cb1684448f5073752dc1
Tatiana Gorelova <gorelovate@yandex.ru>
2024-07-28 11:59:51 +0300

    add: payment method select

---
+++ client/src/entities/PaymentMethod/ui/PaymentMethodSelector/PaymentMethodSelector.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import 'app/styles/index.scss'
+import { WithColorsTheme } from '@/shared/config/storybook/ThemeDecorator/WithColorsTheme'
+import { PaymentMethodSelector } from './PaymentMethodSelector'
+import { paymentMethodsStoreDecorator } from '@/shared/config/storybook/StoreDecorator/PaymentMethodStoreDecorator'
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof PaymentMethodSelector> = {
+    title: 'entities/PaymentMethod/Selector',
+    component: PaymentMethodSelector,
+    argTypes: {
+    // backgroundColor: { control: 'color' },
+    },
+    tags: ['autodocs']
+}
+
+
+export default meta
+type Story = StoryObj<typeof PaymentMethodSelector>;
+
+
+export const Default: Story = {
+    args: {
+        value: '3',
+        onChange: () => {},                                                                                 
+    },
+    decorators: [
+        WithColorsTheme,
+        paymentMethodsStoreDecorator
+    ],
+    parameters: {
+        docs: {
+            canvas: {sourceState: 'shown'}
+        },
+    }
+}
---
+++ client/src/entities/PaymentMethod/ui/PaymentMethodSelector/PaymentMethodSelector.tsx
@@ -0,0 +1,58 @@
+import { memo, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { getPaymentMethodsData } from '../../model/selectors/getPaymentMethodsData/getPaymentMethodsData'
+import cls from './PaymentMethodSelector.module.scss'
+import { AppButton, ButtonTheme } from '@/shared/ui/AppButton/AppButton'
+import { getPaymentMethodById } from '../../model/selectors/getPaymentMethodById/getPaymentMethodById'
+import {  VStack } from '@/shared/ui/Stack'
+import { Input } from '@/shared/ui/Input/Input'
+import { PaymentMethod } from '../../model/types/PaymentMethod'
+
+interface PaymentMethodSelectorProps {
+  value: string,
+  onChange?: (value: string) => void
+}
+
+export const PaymentMethodSelector = memo(({onChange, value}: PaymentMethodSelectorProps) => {
+    const variants = useSelector(getPaymentMethodsData)
+    const [currentMethod, setCurrentMethod] = useState(useSelector(getPaymentMethodById(value)))
+    const {t} = useTranslation()
+    const [visible, setVisible] = useState(false)
+    const [search, setSearch] = useState('')
+
+    const filteredVariants = variants?.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+
+    const handleToggleList = () => {
+        setVisible(!visible)
+    }
+
+    const handleSetSearch = (val: string) => {
+        setSearch(val)
+    }
+
+    const handleChooseVariant = (el: PaymentMethod) => {
+        onChange?.(el._id)
+        setCurrentMethod(el)
+        handleToggleList()
+    }
+    if (!variants) {
+        return <>{t('Loading...')}</>
+    }
+
+    return <fieldset className={cls.group}>
+        <legend className={cls.legend}>{t('Метод оплаты')}</legend>
+        <AppButton stretch={true} theme={ButtonTheme.OUTLINED_LIGHT} onClick={handleToggleList}>{value ? currentMethod?.name : t('Выберите метод оплаты')}</AppButton>
+        <VStack max className={cls.list_wrapper + ' ' + (visible ? '' : cls.hidden )} >
+            <Input label={t('Поиск')} onChange={handleSetSearch}/>
+            {filteredVariants?.map((item) => {
+                return (
+                    <div key={item._id} className={cls.list_item} onClick={() => handleChooseVariant(item)} >
+                        <img src={item.icon_url} alt={item.name}  className={cls.item__image}/>
+                        <span>{item.name}</span>
+                    </div>
+                )
+            })}
+        </VStack>
+    </fieldset>
+})
